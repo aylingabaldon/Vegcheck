465e77f9316bc804690cda0da7005d48dfb76bac
aylingabaldon <aylingabaldon@gmail.com>
2025-11-09 16:25:58 -0600

    Update vegcheck.py

--- vegcheck.py
+++ vegcheck.py
@@ -14,20 +14,18 @@ tabs = st.tabs(["Ingredientes no aptos ❌", "Ingredientes a revisar 🔎", "Pro
 with tabs[0]:
   st.header("Ingredientes no aptos ❌")
   st.write("Aquí encontrarás una lista de ingredientes presentes en los productos que son de origen animal")
-  categoria = st.tabs(["Origen animal directo", "Colorantes o aditivos"])
-with categoria[0]:
-  st.header("Origen animal directo")
-  Ingredientes = ["**Animales:** Vaca, cerdo, pescado, mariscos, cordero, caballo, pollo, gallina, cabra, conejo, pavo, pato, borrego, aves, insectos, entre otros.", "**Gelatina:** Proveniente principalmente de proteína animal como piel o huesos.", "**Caseína:** Es la principal proteína de la leche.",
+
+  Ingredientes = ["**Animales:** Vaca, cerdo, pescado, mariscos, cordero, caballo, pollo, gallina, cabra, conejo, pavo, pato, borrego, aves, insectos, entre otros.", "**Gelatina o E441:** Proveniente principalmente de proteína animal como piel o huesos.", "**Caseína:** Es la principal proteína de la leche.",
                   "**Caseínato:** Es derivado de la caseína y por lo tanto de la leche, usado como espesante.", "**Caseínato de sodio:** Es una sal sódica que se obtiene de la leche al precipitar las proteínas y neutralizarlas con hidróxico de sodio. Usado como espesante, aglutinante o fuente de proteínas.",
                  "**Suero de leche:** Es el líquido amarillento que queda después de que se coagula la leche. Usado principalmente en quesos y yogurt.", "**Lactosa:** Es el azúcar natural que se encuentra presente en la leche.", 
                  "**Miel:** Sustancia natural proveniente y producida por las abejas.", "**Propóleo:** Es una sustancia resinosa que fabrican las abejas a partir de los árboles, usado como conservante en los alimentos.", 
-                 "**Jalea real:** Sustancia secretada por las abejas de color oscuro. Usado como complemento al aportar energía y nutrientes.", "**Carmín:** Es un colorante rojo intenso extraído principalmente de la cochinilla", 
+                 "**Jalea real:** Sustancia secretada por las abejas de color oscuro. Usado como complemento al aportar energía y nutrientes.", "**Carmín o E120:** Es un colorante rojo intenso extraído principalmente de la cochinilla", 
                  "**Isinglass:** Es una gelatina traslúcida hecha con las colas de pescado secas. Utilizado para la clarificación de cervezas y vinos.", "**Albúmina de huevo:** Es las proteína principal de la clara de huevo, utilizada como espumante o estabilizador.",
                  "**Albumen:** Es otro nombre con el cual se nombra a la clara del huevo.", "**Lactoalbúmina:** Es una proteína del suero de la leche. Utilizada en fórmulas infantiles para imitar la leche materna o en suplementos.", 
                  "**Shellac o E904:** Es un barniz de origen natural producido por el insecto de la laca. Utilizado para otorgar un glaseado, recubrimiento o brillo en los alimentos.", 
                  "**Cera de abejas o E901:** Sustancia natural producida por las abejas para construir sus colmenas, se utilizan como agente de recubrimiento en algunos alimentos.", "**Manteca de cerdo:** Es la grasa de cerdo derretida, utilizado como aceite para freír, guisar o preparar masas.",
                  "**Sebo:** Grasa sólida de los animales que se utiliza como grasa para cocinar, alimentos para mascotas e incluso en velas, jabones o lubricantes.", "**Lactitol o E966:** Es un polialcohol derivado de la lactosa, que se utiliza principalmente como edulcorante bajo en calorías.",
-                 "**Lanolina:** Es una sustancia cerosa y oleosa que se extrae de la lana de la obeja, utilizada en productos cosméticos y cuidados de la piel."]
+                 "**Lanolina o E913:** Es una sustancia cerosa y oleosa que se extrae de la lana de la obeja, utilizada en productos cosméticos y cuidados de la piel.", "**Fosfato de hueso comestible o E542:** Es un aditivo alimentario utilizado como antiaglomerante que proviene de huesos de animalescomo el vacuno o porcino. Tambien se puede encontrar en pastas de dientes o suplementos."]
   buscar = st.text_input("Busca el ingrediente ⌕")
   if buscar:
     resultado = [p for p in Ingredientes if buscar.lower() in p.lower()]
@@ -40,14 +38,4 @@ with categoria[0]:
   st.subheader("Lista completa de los ingredientes 📝")
   for Ingredientes in Ingredientes:
     st.write(f"➤ {Ingredientes}")
-with categoria[1]:
-  st.header("Colorantes o aditivos")
-  Colorante = []
-  busqueda = st.text_input("Busca el número ⌕")
-  if buscar:
-    resultados = [p for p in Colorante if buscar.lower() in p.lower()]
-    if resultados:
-      st.write("Resultados: ")
-      for r in resultados:
-        st.write(f"- {r}")
-  
+
