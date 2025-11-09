458a2cc21a83b21e8d0b7ef95e6fafea3a2b154c
aylingabaldon <aylingabaldon@gmail.com>
2025-11-09 14:35:26 -0600

    Update vegcheck.py

--- vegcheck.py
+++ vegcheck.py
@@ -16,7 +16,7 @@ with tabs[0]:
   categoria = st.tabs(["Origen animal directo", "Colorantes o aditivos"])
 with categoria[0]:
   st.header("Origen animal directo")
-  Ingredientes = ["Gelatina: Proveniente principalmente de proteína animal como piel o huesos", "Caseína: Es la principal proteína de la leche"]
+  Ingredientes = ["**Gelatina:** Proveniente principalmente de proteína animal como piel o huesos", "**Caseína:** Es la principal proteína de la leche"]
   for Ingredientes in Ingredientes:
     st.write(f"➤ {Ingredientes}")
                           
