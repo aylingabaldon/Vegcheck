193e74fcb9f41b970ee355a2640399532e012476
aylingabaldon <aylingabaldon@gmail.com>
2025-11-14 21:02:04 -0600

    Update vegcheck.py

--- vegcheck.py
+++ vegcheck.py
@@ -44,7 +44,16 @@ with tabs[0]:
   st.subheader("¿Cómo sustituyo los productos de origen animal?")
 # st.write(AQUI ESCRIBIR UNA BREVE INTRODUCCIÓN LAS ALTERNATIVAS QUE SE TIENE DE LOS ALIMENTOS BÁSICOS DE ORIGEN ANIMAL A LOS DE ORIGEN VEGETAL)
 # INCLUIR IMAGENES DE LOS PRODUCTOS
+
+  # ESTE ES UN EJEMPLO DE COMO SE DEBE DE PONER CADA PRODUCTO, SE PUEDE COPIAR Y PEGAR PARA QUE PONGAN TODAS LAS CATEGORÍAS
   st.write("**LECHE**")
+# st.write("Incluir una breve descripción")
+  col1, col2 = st.columns(2)
+  with col1:
+    st.image("https://static.independent.co.uk/2024/04/12/13/Vegan-milks-hero.png?fit=crop&height=630&width=1200")
+  with col2:
+    st.image("https://hebmx.vtexassets.com/arquivos/ids/703414-800-800?v=638521750175800000&width=800&height=800&aspect=true")
+ 
   col1, col2 = st.columns(2)
   with col1:
     st.image("https://static.independent.co.uk/2024/04/12/13/Vegan-milks-hero.png?fit=crop&height=630&width=1200")
