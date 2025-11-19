3adcf6bf5400f0dcca0a15b742544f7d44e28efd
Sheenn0 <sheenn0@yahoo.com.mx>
2025-11-19 04:18:56 -0600

    Update vegcheck.py

--- vegcheck.py
+++ vegcheck.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-st.image("VegCheck app icon.png", width=100)
+st.image("VegCheck app icon.png", width=200)
 st.title("VegCheck 🌱💚")
 st.header("¿Eres vegano y batallas para encontrar productos 100% aptos para ti?")
 st.write("Entonces esta app es ideal para ti, aquí encontrarás información importante que te ayudará a elegir los productos que cumplan con tu estilo de vida")
