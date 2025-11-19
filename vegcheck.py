5f6ad20182b36a062797369544082c2232c8edbe
Sheenn0 <sheenn0@yahoo.com.mx>
2025-11-18 23:12:45 -0600

    Update vegcheck.py
    Logos añadidos

--- vegcheck.py
+++ vegcheck.py
@@ -3,6 +3,7 @@ import streamlit as st
 st.title("VegCheck 🌱💚")
 st.header("¿Eres vegano y batallas para encontrar productos 100% aptos para ti?")
 st.write("Entonces esta app es ideal para ti, aquí encontrarás información importante que te ayudará a elegir los productos que cumplan con tu estilo de vida") 
+st.image("https://uach.mx/assets/media/publications/2017/11/169_imagen-uach/escudo-color.png")
 st.sidebar.write("**VEGCHECK:** Una app para tu nuevo estilo de vida")
 st.sidebar.write("**Equipo:** ")
 Nombres = ["Paola Conde", "Aylín Gabaldón", "José Luis Guevara", "Alan López", "Luis Yepiz"]
@@ -10,6 +11,7 @@ for Nombres in Nombres:
   st.sidebar.write(f"• {Nombres}")
 st.sidebar.write("**Materia:** Programación")
 st.sidebar.write("**Grupo:** 3L")
+st.image("https://uach.mx/assets/media/publications/2022/11/4120_simbolos-universitarios/Escudo.png")
 tabs = st.tabs(["Iniciación al veganismo👩🏻‍💻👨🏾‍💻", "Ingredientes no aptos ❌", "Ingredientes a revisar 🔎", "Productos 100% veganos ✅", "Recetas Veganas 🍽️"])
 
 with tabs[0]:
